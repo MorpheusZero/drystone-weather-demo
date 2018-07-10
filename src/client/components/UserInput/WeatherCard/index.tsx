5c7903af94274339f7920558e8aff9d95a191d23
Dylan Legendre <dylanlegendre09@gmail.com>
2018-07-09 22:19:29 -0500

    first working main version

--- src/client/components/UserInput/WeatherCard/index.tsx
+++ src/client/components/UserInput/WeatherCard/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import config from '../../../../config/config';
+import * as moment from 'moment';
 
 /**
  * Handles the main view and user input for the weather card details.
@@ -14,13 +15,19 @@ export class WeatherCard extends React.Component<{ weatherDetails: any}, any> {
         super(props);
     }
 
-    
-    private buildConditionsCard(name: string, main: any, wind: any, weather: any): string {
+    /**
+     * Builds the container for the current conditions card.
+     * @param name 
+     * @param main 
+     * @param wind 
+     * @param weather 
+     */
+    private buildCurrentConditionsCard(name: string, main: any, wind: any, weather: any): string {
         const iconPath: string = config.iconBaseUrl.replace('{ICON}', weather[0].icon);
         return `
-        <div class="">
-            <p>${name}</p>
-            <img src="${iconPath}"></img>
+        <div class="condition-card">
+            <h4 class="center">${name}</h4>
+            <p class="center"><img src="${iconPath}"></img></p>
             <p>Wind: ${wind.speed}</p>
             <p>Humidity: ${main.humidity}</p>
             <p>Current Temp: ${main.temp}</p>
@@ -30,8 +37,33 @@ export class WeatherCard extends React.Component<{ weatherDetails: any}, any> {
         `;
     }
 
+    private buildForecastCard(name: string, list: Array<any>): string {
+        let daysReplaceString = '';
+        for (let i = 1; i <= 40; i = i + 8) {
+            const iconPath: string = config.iconBaseUrl.replace('{ICON}', list[i].weather[0].icon);
+            daysReplaceString += `
+            <p>
+            ${moment(list[i].dt_txt).format("MMM Do YY")}
+            <br />
+            <img src="${iconPath}"></img> H:${list[i].main.temp_max} L:${list[i].main.temp_min}
+            </p>
+            `;
+        }
+        return `
+        <div class="condition-card">
+            <h4 class="center">${name}</h4>
+            ${daysReplaceString}
+        </div>
+        `;
+    }
+
     public render(): any {
-        return (<div className="form-group" dangerouslySetInnerHTML={{__html: (this.props.weatherDetails && this.props.weatherDetails.current) ? this.buildConditionsCard(this.props.weatherDetails.current.name, this.props.weatherDetails.current.main, this.props.weatherDetails.current.wind, this.props.weatherDetails.current.weather) : ''}}></div>)
+        return (
+        <span>
+            <div className="form-group block-margin--right" dangerouslySetInnerHTML={{__html: (this.props.weatherDetails && this.props.weatherDetails.current) ? this.buildCurrentConditionsCard(this.props.weatherDetails.current.name, this.props.weatherDetails.current.main, this.props.weatherDetails.current.wind, this.props.weatherDetails.current.weather) : ''}}></div>
+            <div className="form-group block-margin--right" dangerouslySetInnerHTML={{__html: (this.props.weatherDetails && this.props.weatherDetails.fiveDay) ? this.buildForecastCard(this.props.weatherDetails.fiveDay.city.name, this.props.weatherDetails.fiveDay.list) : ''}}></div>
+        </span>
+    )
     }
 
 }
